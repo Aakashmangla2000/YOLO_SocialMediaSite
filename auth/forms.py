047a1372c331865e4f775f492a8514a3c01eb50b
Aakash Mangla <akkibaby90@yahoo.com>
2020-09-20 01:20:58 +0530

    Improved front end

--- auth/forms.py
+++ auth/forms.py
@@ -5,7 +5,6 @@ class SignUpForm(UserCreationForm):
     email = forms.EmailField()
     first_name = forms.CharField()
     last_name = forms.CharField()
-    
     class Meta:
         model = User
         fields = ( 'first_name', 'last_name', 'email', 'username', 'password1', 'password2')
